c4694f5afe696c850f0ac2500ac4d0137e7baf9a
uascasibar25bj <uascasibarli25bj@ikzubirimanteo.com>
2025-11-10 09:29:16 +0100

    Pistolak tiro egitean soinua entzun eta kolisio puntuan "marka"/partikula jarri

--- Assets/Scripts/RayGun.cs
+++ Assets/Scripts/RayGun.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class RayGun : MonoBehaviour
@@ -5,9 +6,12 @@ public class RayGun : MonoBehaviour
     public LayerMask layermask;
     public OVRInput.RawButton shootingButton;
     public LineRenderer linePrefab;
+    public GameObject rayImpactPrefab;
     public Transform shootingPoint;
     public float maxLineDistance = 5;
     public float lineShowerTimer = 0.3f;
+    public AudioSource source;
+    public AudioClip shootingAudioClip;
 
     void Start()
     {
@@ -25,14 +29,28 @@ public class RayGun : MonoBehaviour
     
     public void Shoot()
     {
+        source.PlayOneShot(shootingAudioClip);
         Ray ray = new Ray(shootingPoint.position, shootingPoint.forward);
         bool hasHit = Physics.Raycast(ray, out RaycastHit hit, maxLineDistance, layermask);
+
+        Vector3 endPoint = Vector3.zero;
+
+        if (hasHit)
+        {
+            endPoint = hit.point;
+
+            Quaternion rayImpactRotation = Quaternion.LookRotation(-hit.normal);
+            GameObject rayImpact = Instantiate(rayImpactPrefab, hit.point, rayImpactRotation);
+            Destroy(rayImpact, 1);
+        }
+        else
+        {
+            endPoint = shootingPoint.position + shootingPoint.forward * maxLineDistance;
+        }
 
         LineRenderer line = Instantiate(linePrefab);
         line.positionCount = 2;
         line.SetPosition(0, shootingPoint.position);
-
-        Vector3 endPoint = shootingPoint.position + shootingPoint.forward * maxLineDistance;
 
         line.SetPosition(1, endPoint);
 
